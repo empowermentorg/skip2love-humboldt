48e55dc17d36184f64ba53ac1c1444d28c6d680b
root <root@localhost.localdomain>
2025-06-26 11:30:43 +0000

    Save current progress before debugging navigation

---
+++ src/supabaseClient.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://efpeuaqgijrbxvlbjgga.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmcGV1YXFnaWpyYnh2bGJqZ2dhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA0ODM4NTMsImV4cCI6MjA2NjA1OTg1M30.HnHjGUcYdmBcMwnzifHouOYrG9q72MuhtcH8svJ44_4';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
